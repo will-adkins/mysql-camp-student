3416a703e8f42cbd5ae67dc59be128a49e9f99e5
Jared Bilton <jsbilton@gmail.com>
2016-11-09 12:00:58 -0500

    exercise2_completed

--- student-exercises/bilton-jared-select.sql
+++ student-exercises/bilton-jared-select.sql
@@ -1,5 +1,7 @@
 USE RockStarDay2;
 
+-- Excercise 1 --
+
 SELECT * FROM Individual
 WHERE LastName = 'Jennings';
 
@@ -15,3 +17,12 @@ WHERE Year(BirthDate) > 1940;
 
 SELECT * FROM Individual
 WHERE ID IN (1,3,5,7,19);
+
+
+-- Exercise 2 --
+
+SELECT * FROM Band
+WHERE Genre = 'Alternative' and isTogether = 0;
+
+SELECT ID, Name FROM Band
+WHERE ID = 4;
