631ed88d9e579a8641dfb62afcc2971e6b59b284
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-11-09 11:48:56 -0500

    added more queries

--- student-exercises/smith-select.sql
+++ student-exercises/smith-select.sql
@@ -9,3 +9,7 @@ SELECT ID, LASTNAME FROM Individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID, LastName, BirthDate FROM Individual WHERE Year(BirthDate) > 1940;
 
 SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
+
+SELECT * FROM Band WHERE Genre = 'Alternative' and IsTogether = 0;
+
+SELECT ID, Name FROM Band WHERE IS = 4;
