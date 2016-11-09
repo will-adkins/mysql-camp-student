66b237b00b73c90c11532673e8edec863f409d70
garudacrafts <derek@garudacrafts.com>
2016-11-09 13:44:08 -0500

    step 5 completed

--- student-exercises/garudacrafts-select.sql
+++ student-exercises/garudacrafts-select.sql
@@ -24,3 +24,13 @@ SELECT * FROM Band WHERE Genre = 'Alternative' AND IsTogether = 0;
 #Create a query that provides the id and name of the band with an ID of 4
 SELECT ID, Name FROM Band WHERE ID = 4;
 
+# Exercise 5
+
+# Find all bands that have an 'and' in the middle of their band name like 'Jason and the Scorchers'.
+SELECT * FROM Band WHERE Name LIKE '% and %';
+
+# Find people who have a three letter first name that is like 'Tim' or 'Kim' or 'Jim'.
+SELECT * FROM Individual WHERE FirstName LIKE '___';
+
+# Find all bands that end with 's' like 'The Beatles' or 'The Rolling Stones' or 'Pixies'.
+SELECT * FROM Band WHERE Name LIKE '%s';
