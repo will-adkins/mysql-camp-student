841ed47cc510fac434ba1123ad4b9481911f2823
chriscortes4u <chriscortes4u@yahoo.com>
2016-11-09 13:44:30 -0500

    Adding ex numb 3

--- student-exercises/cortes4u-select.sql
+++ student-exercises/cortes4u-select.sql
@@ -14,3 +14,19 @@ SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
 SELECT * FROM Band WHERE IsTogether = '0' and Genre = 'Alternative';
 
 SELECT ID, Name FROM Band WHERE ID = 4;
+
+
+/* exercise 3 */
+
+
+SELECT * FROM Band WHERE Name LIKE ‘The%’;
+
+SELECT * FROM Band WHERE Name LIKE ‘%Stones’;
+
+SELECT * FROM Individual WHERE FirstName Like ‘__ck’;
+
+SELECT * FROM Band WHERE Name LIKE '%and%';
+
+SELECT * FROM Individual WHERE FirstName Like '___';
+
+SELECT * FROM Band WHERE Name LIKE '%s';
