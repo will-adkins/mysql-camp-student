e82391ebe5b81d90c7e26d641bb669f9552495e0
chriscortes4u <chriscortes4u@yahoo.com>
2016-11-09 13:17:36 -0500

    adding ecercise 2

--- student-exercises/cortes4u-select.sql
+++ student-exercises/cortes4u-select.sql
@@ -1,4 +1,5 @@
 USE RockStarDay2;
+/* exercise 1 */
 
 SELECT * FROM Individual WHERE LastName = 'Jennings';
 
@@ -7,3 +8,9 @@ SELECT ID, LASTNAME FROM Individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID, LastName, BirthDate FROM Individual WHERE Year(BirthDate) > 1940;
 
 SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
+
+/* exercise 2 */
+
+SELECT * FROM Band WHERE IsTogether = '0';
+
+SELECT ID, Name FROM Band WHERE ID =4;
