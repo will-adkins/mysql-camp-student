74f9e56334162cfd482d455b713f4ff068315c10
Matt Theodore <mtheodo@Matthews-MacBook-Air.local>
2016-11-09 13:10:38 -0500

    completion exercise 4

--- student-exercises/theodore-select.sql
+++ student-exercises/theodore-select.sql
@@ -12,3 +12,9 @@ WHERE Year(BirthDate) > 1940;
 
 SELECT * FROM Individual
 WHERE ID IN (1,3,5,7,19);
+
+SELECT * FROM Band
+WHERE Genre = 'Alternative' and IsTogether = 0;
+
+SELECT ID, Name FROM Band
+WHERE IS = 4;
