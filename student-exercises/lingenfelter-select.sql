e248498cd52891f8a8dc990f143b0e459383e5ff
Andrea <alingenfelter2003@gmail.com>
2016-11-09 11:56:48 -0500

    exercise 4

--- student-exercises/lingenfelter-select.sql
+++ student-exercises/lingenfelter-select.sql
@@ -12,3 +12,7 @@ mysql> select ID, LastName, BirthDate from Individual where Year(BirthDate) > 19
 
 
 mysql> select * from Individual where ID in (1,3,5,7,19);
+
+mysql> select Name, YearFormed from Band where IsTogether = 0 and Genre = 'alternative';
+
+mysql> select ID, Name from Band where ID = 4;
