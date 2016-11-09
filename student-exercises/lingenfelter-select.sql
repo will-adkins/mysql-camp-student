d2a16f2e2384e8b367edfdd0729ce7792837e051
Andrea <alingenfelter2003@gmail.com>
2016-11-09 16:49:01 -0500

    JOINS ex 1

--- student-exercises/lingenfelter-select.sql
+++ student-exercises/lingenfelter-select.sql
@@ -24,3 +24,8 @@ select * from Individual where FirstName like '_ck';
 select * from Band where Name like '% and %';
 select * from Individual where FirstName like '_im';
 select * from Band where Name like '%s';
+
+-- ex 1 JOINS
+select b.playerID, p.FirstName, p.LastName, b.battingAvg from batting as b inner join player as p where b.playerID = p.ID and b.battingAvg = 331;
+select p.*, r.* from player as p inner join roster as r on p.ID = r.PlayerID;
+select p.*, r.*, t.* from player as p inner join roster as r on p.ID = r.PlayerID inner join team as t on r.teamID = t.ID where t.teamName = 'Boston Red Sox';
