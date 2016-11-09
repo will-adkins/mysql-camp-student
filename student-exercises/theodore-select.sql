5df5fd18a5ebdbdbb3dd5304bb5a18cab0c1ab8a
Matt Theodore <mtheodo@Matthews-MacBook-Air.local>
2016-11-09 13:50:47 -0500

    commit exc 5

--- student-exercises/theodore-select.sql
+++ student-exercises/theodore-select.sql
@@ -17,4 +17,20 @@ SELECT * FROM Band
 WHERE Genre = 'Alternative' and IsTogether = 0;
 
 SELECT ID, Name FROM Band
-WHERE IS = 4;
+WHERE ID= 4;
+
+SELECT YearFormed, COUNT(ID) as BandCount FROM Band
+Group BY YearFormed
+HAVING BandCount > 1;
+
+Select ID, Name, Genre as BandGenre
+FROM Band;
+
+SELECT ID, Name FROM Band
+WHERE Name LIKE 'Th_ Band';
+
+SELECT * FROM Band
+WHERE Name LIKE '% and %';
+
+SELECT * FROM Individual
+WHERE FirstName LIKE '_im';
