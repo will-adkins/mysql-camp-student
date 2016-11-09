f05b3f2631d5866680bb45f3fd71953e2fb49121
garudacrafts <derek@garudacrafts.com>
2016-11-09 11:54:42 -0500

    step 3 completed

--- student-exercises/garudacrafts-select.sql
+++ student-exercises/garudacrafts-select.sql
@@ -3,3 +3,10 @@ SHOW COLUMNS FROM Band;
 
 SELECT ID, Name, Genre FROM Band;
 
+SELECT * FROM Individual WHERE LastName = 'Jennings';
+
+SELECT ID, LASTNAME FROM Individual WHERE DeceasedDate IS NOT NULL;
+
+SELECT ID, LastName, BirthDate FROM Individual WHERE Year(BirthDate) > 1940;
+
+SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
