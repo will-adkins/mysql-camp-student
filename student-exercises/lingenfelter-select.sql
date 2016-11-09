1dbb346de987679b9c3295bccac144a6754fd78c
Andrea <alingenfelter2003@gmail.com>
2016-11-09 13:46:15 -0500

    ex 5

--- student-exercises/lingenfelter-select.sql
+++ student-exercises/lingenfelter-select.sql
@@ -7,7 +7,7 @@ mysql> select * from Individual where LastName = 'Jennings';
 
 mysql> select ID, LastName from Individual where DeceasedDate is not null;
 
-
+--ex 4
 mysql> select ID, LastName, BirthDate from Individual where Year(BirthDate) > 1940;
 
 
@@ -16,3 +16,11 @@ mysql> select * from Individual where ID in (1,3,5,7,19);
 mysql> select Name, YearFormed from Band where IsTogether = 0 and Genre = 'alternative';
 
 mysql> select ID, Name from Band where ID = 4;
+
+-- ex 5
+select * from Band where Name like 'The%';
+select * from Band where Name like '%Stones';
+select * from Individual where FirstName like '_ck';
+select * from Band where Name like '%and%';
+select * from Band where Name like '___';
+select * from Band where Name like '%s';
