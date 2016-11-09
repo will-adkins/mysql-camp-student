08e81799feff866f880cfc155eb44ed682540c46
aplchian <alex.boquis@gmail.com>
2016-11-09 11:51:40 -0500

    added 2 sql statements

--- student-exercises/aplchian-select.sql
+++ student-exercises/aplchian-select.sql
@@ -6,3 +6,5 @@ SELECT * FROM Individual WHERE LastName='Jennings';
 SELECT ID, LASTNAME FROM Individual WHERE DeceasedDate IS NOT NULL;
 SELECT ID, LASTNAME, BIRTHDATE FROM Individual WHERE Year(Birthdate) > 1940;
 SELECT * FROM Individual WHERE ID IN (1,3,5,7,19);
+SELECT * FROM Band WHERE Genre = "Alternative" AND IsTogether = false;
+SELECT ID, Name FROM Band WHERE ID = "4";
