f178e6487d5aaccc3fb75c2ea56c8c850b08d3bc
Andrea <alingenfelter2003@gmail.com>
2016-11-09 11:51:43 -0500

    exercise 3

--- student-exercises/lingenfelter-select.sql
+++ student-exercises/lingenfelter-select.sql
@@ -1,3 +1,14 @@
 use RockStarDay2;
 
 SHOW COLUMNS FROM Band;
+
+mysql> select * from Individual where LastName = 'Jennings';
+
+
+mysql> select ID, LastName from Individual where DeceasedDate is not null;
+
+
+mysql> select ID, LastName, BirthDate from Individual where Year(BirthDate) > 1940;
+
+
+mysql> select * from Individual where ID in (1,3,5,7,19);
