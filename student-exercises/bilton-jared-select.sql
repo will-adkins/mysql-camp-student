4d34ff14757374ed06e9e9773b53aa111273a7c2
Jared Bilton <jsbilton@gmail.com>
2016-11-09 11:54:43 -0500

    exercise1_completed

--- student-exercises/bilton-jared-select.sql
+++ student-exercises/bilton-jared-select.sql
@@ -2,3 +2,16 @@ USE RockStarDay2;
 
 SELECT * FROM Individual
 WHERE LastName = 'Jennings';
+
+SELECT ID, LastName
+FROM Individual
+WHERE DeceasedDate IS NOT NULL;
+
+
+SELECT ID, LastName, BirthDate
+FROM Individual
+WHERE Year(BirthDate) > 1940;
+
+
+SELECT * FROM Individual
+WHERE ID IN (1,3,5,7,19);
