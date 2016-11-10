22af9176f7aeeabfc1084e645d9993a037262990
Jared Bilton <jsbilton@gmail.com>
2016-11-09 22:02:40 -0500

    exercise_10_join_part_1

--- student-exercises/bilton-jared-select.sql
+++ student-exercises/bilton-jared-select.sql
@@ -37,8 +37,60 @@ FROM Band
 WHERE Name LIKE '%and%';
 
 SELECT *
-FROM Individual WHERE FirstName LIKE '_im';
+FROM Individual
+WHERE FirstName LIKE '_im';
 
 SELECT *
 FROM Band
 WHERE Name LIKE '%s';
+
+
+-- Exercise 10-joins/1 --
+Select * from player;
+Select * from team;
+Select * from batting;
+Select * from roster;
+
+SELECT team.ID
+, team.TeamName
+, batting.TeamID
+, batting.Rank
+, batting.BattingAvg
+FROM team
+INNER JOIN batting ON team.ID = batting.TeamID;
+
+SELECT player.ID
+ , player.LastName
+ , batting.Rank
+ , batting.BattingAvg
+ FROM player
+ INNER JOIN batting ON player.ID = batting.PlayerID;
+
+ SELECT player.ID
+ , player.LastName
+ , batting.Rank
+ , batting.BattingAvg
+ FROM player
+ INNER JOIN batting ON player.ID = batting.PlayerID
+ WHERE batting.BattingAvg = 331;
+
+ SELECT roster.*
+, player.LastName
+, player.ID
+FROM roster
+INNER JOIN player ON roster.playerID = player.ID;
+
+SELECT * FROM vteamRoster;
+
+DESCRIBE vteamRoster;
+
+SHOW CREATE VIEW vteamRoster;
+
+
+-- Joins Exercise 2 --
+
+-- Create a SELECT statement that joins the **player** table and the **batting** table. The query should show all the players and any matching players that exist within the batting table. You should see NULL when there are no matches in the batting table.
+
+SELECT player
+
+LEFT JOIN player
