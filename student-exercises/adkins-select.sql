b14e5edb0be93c99177dc85262dca0c7a551b7eb
Will Adkins <wkadki01@gmail.com>
2018-06-28 10:29:33 -0400

    more queries pt deux

--- student-exercises/adkins-select.sql
+++ student-exercises/adkins-select.sql
@@ -2,3 +2,11 @@ USE rockStarDay2;
 SHOW COLUMNS FROM band;
 
 select ID, name, genre from band;
+
+select * from individual where lastName = 'Jennings';
+
+select ID, lastName from individual where deceasedDate is not null;
+
+select ID, lastName, birthDate from individual where Year(birthDate) > 1940;
+
+select * from individual where id in (1,3,5,7,19);
