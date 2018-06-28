0fe1ae0750dc9d8ae7fa0321f72eae35fdedd242
Will Adkins <wkadki01@gmail.com>
2018-06-28 15:07:43 -0400

    Wildcard queries pt. 1

--- student-exercises/adkins-select.sql
+++ student-exercises/adkins-select.sql
@@ -14,3 +14,24 @@ select * from individual where id in (1,3,5,7,19);
 select * from band where genre = 'Alternative' and isTogether = 0;
 
 select ID, name from band where ID = 4;
+
+-- show the names and band of all the individuals who are in alternative bands formed after 1970
+
+select individual.firstName, individual.lastName, band.name
+from individualBand
+join band on individualBand.bandID = band.ID
+join individual on individualBand.individualID = individual.ID
+where band.genre = 'Rock' and band.yearFormed > '1970';
+
+select * from band
+where name LIKE '% and %';
+ 
+ select * from individual
+ where firstName LIKE 'Tim%' 
+ or firstName LIKE 'Kim%'
+ or firstName LIKE 'Jim%'
+ 
+ select * from band
+ where name LIKE '%s';
+ 
+ 
